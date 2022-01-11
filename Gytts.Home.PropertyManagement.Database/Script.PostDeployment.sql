7946cfad88e513b63fd9ec8c0a3929b2a3b564d7
Soroush Saghafi-Khadem <soroush.saghafi@gmail.com>
2022-01-11 18:43:52 -0500

    Publish profiles reviewed

--- Gytts.Home.PropertyManagement.Database/Script.PostDeployment.sql
+++ Gytts.Home.PropertyManagement.Database/Script.PostDeployment.sql
@@ -2,5 +2,6 @@
 begin
 	insert into dbo.[Property] (Name, Address)
 	values ('Home #1', 'Home Adrress #1'),
-	('Home #2', 'Home Adrress #2');
+	('Home #2', 'Home Adrress #2'),
+    ('Home #3', 'Home Adrress #3');
 end
--- Gytts.Home.PropertyManagement.Database/dbo/Tables/Property.sql
+++ Gytts.Home.PropertyManagement.Database/dbo/Tables/Property.sql
@@ -2,5 +2,5 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NOT NULL, 
-    [Address] NVARCHAR(50) NOT NULL
+    [Address] NVARCHAR(50) NOT NULL 
 )
